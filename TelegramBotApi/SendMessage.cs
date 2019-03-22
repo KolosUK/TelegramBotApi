17a7b49e5f1fed4c1543e50243447ca71104ed0f
Pavlo Didyk <pavlo.didyk@jetsoftpro.com>
2019-03-22 15:44:41 +0200

    Add new methods

--- TelegramBotApi/SendMessage.cs
+++ TelegramBotApi/SendMessage.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.IO;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace TelegramBotApi
 {
@@ -51,6 +54,53 @@ namespace TelegramBotApi
                         webClient.UploadValues(Url + Token + "/sendMessage", pars);
                     }
 
+                    if ( Message.ToLower().Contains("артуррр") ) {
+                        pars.Add("chat_id", ChatID);
+                        pars.Add("text", "Сіські!!!");
+                        webClient.UploadValues(Url + Token + "/sendMessage", pars);
+                    }
+
+                    if ( Message.ToLower().Contains("артуррр") ) {
+                        List<string> result = new List<string>();
+                        using ( StreamReader r = new StreamReader("../../../StringsImg.json") ) {
+                            var json = r.ReadToEnd();
+                            var jobj = JObject.Parse(json);
+                            foreach ( var item in jobj.Properties()) {
+                                foreach ( var img in item.Value ) {
+                                    result.Add(img.ToString());
+                                }                        
+                            }
+                        }
+                        Random rnd = new Random();
+                        int photoId = rnd.Next(result.Count);
+                        string randomImg = result[photoId];
+                        pars.Add("chat_id", ChatID);
+                        pars.Add("photo", randomImg);
+                        pars.Add("caption", "");
+                        webClient.UploadValues(Url + Token + "/sendPhoto", pars);
+                    }
+
+                    if ( Message.Contains("Я жму") | Message.Contains("я жму") ) {
+                        pars.Add("chat_id", ChatID);
+                        var resultString = Regex.Match(Message, @"\d+").Value;
+                        if ( Decimal.TryParse(resultString, out var number) ) {
+                            if ( number < 80 ) {
+                                pars.Add("text", "Дрищ йди в зал і нормально займайся блеат!!!");
+                            } else if ( number >= 80 && number <= 120 ) {
+                                pars.Add("text", "Не плохо але їбош дальше");
+                            } else if ( number >= 121 && number <= 150 ) {
+                                pars.Add("text", "Ти машина");
+                            } else if ( number > 151 ) {
+                                pars.Add("text", "Ну ти і піздюк");
+                            }
+
+                        } else {
+                            pars.Add("text", "Напиши нормально і не вимахуйся");
+                        }
+  
+                        webClient.UploadValues(Url + Token + "/sendMessage", pars);
+                    }
+
                     if (Message.Contains("Хто"))
                     {
                         string[] pepole = {"Колос", "Сива", "Ніка", "Вуйко Тарас", "Володька", "Качетко", "Пелишка", "Макар", "Стьопа", "Жора", "Рижулька", "Софа Карпишин", "Джуля", "Римар", "Чопік", "Єнот", "Маша", "Серго", "Самий сексуальний Бодя", "Шаріф", "Чурка" };
@@ -63,7 +113,7 @@ namespace TelegramBotApi
                     if (Message.Contains("Команди"))
                     {
                         pars.Add("chat_id", ChatID);
-                        pars.Add("text", "Верогідність(вираховує вирогідність виникнення події), Чи(чи певна подія відбудетсья), Слава Україні, Хто(вираховує хто той самий)");
+                        pars.Add("text", "Верогідність(вираховує вирогідність виникнення події), Чи(чи певна подія відбудетсья), Слава Україні, Хто(вираховує хто той самий), Я жму ( пишеш скільки кг і він пизначає хто ти є в залі), Артуррр(Фіча для Качетка)");
                         webClient.UploadValues(Url + Token + "/sendMessage", pars);
                     }
                 }
